4d0fe83d2cbc8112f361ca5580208d6961f6797c
Vladimir Rudnevskiy <v.rud.frombts12@gmail.com>
2024-12-11 11:40:07 +0300

    lab 5 Rudnevskij

--- cvlib/src/corner_detector.cpp
+++ cvlib/src/corner_detector.cpp
@@ -7,6 +7,10 @@
 #include "cvlib.hpp"
 #include <ctime>
 
+#include <algorithm>
+#include <random>
+#include <vector>
+
 namespace cvlib
 {
 // static
@@ -15,20 +19,41 @@ cv::Ptr<corner_detector_fast> corner_detector_fast::create()
     return cv::makePtr<corner_detector_fast>();
 }
 
+cv::Point generate_point(int sigma)
+{
+    // std::default_random_engine generator;
+    std::random_device rd{};
+    std::mt19937 generator{rd()};
+
+    std::normal_distribution<float> distribution_x(0, sigma);
+    int x = std::round(distribution_x(generator));
+
+    std::normal_distribution<float> distribution_y(x, sigma);
+    int y = std::round(distribution_y(generator));
+
+    return {std::clamp(x, -sigma, sigma), std::clamp(y, -sigma, sigma)};
+}
+
+void make_point_pairs(std::vector<std::pair<cv::Point, cv::Point>>& pairs, const int desc_length, const int neighbourhood_size)
+{
+    pairs.clear();
+    for (int i = 0; i < desc_length; i++) pairs.push_back(std::make_pair(generate_point(neighbourhood_size / 2),
+                                                                         generate_point(neighbourhood_size / 2)));
+}
+
 void corner_detector_fast::detect(cv::InputArray image, CV_OUT std::vector<cv::KeyPoint>& keypoints)
 {
     keypoints.clear();
     cv::Mat img = image.getMat();
-    if (img.channels() > 1) cv::cvtColor(img, img, cv::COLOR_RGB2GRAY);
+    if (img.channels() > 1)  cv::cvtColor(img, img, cv::COLOR_RGB2GRAY);
 
     const int threshold = 30, t = 9;
-    std::vector<cv::Point> offsets = {cv::Point(0, 3),  cv::Point(1, 2),   cv::Point(2, 2),   cv::Point(3, 1),
-                                      cv::Point(3, 0),  cv::Point(3, -1),  cv::Point(2, -2),  cv::Point(1, -3),
-                                      cv::Point(0, -3), cv::Point(-1, -3), cv::Point(-2, -2), cv::Point(-3, -1),
-                                      cv::Point(-3, 0), cv::Point(-3, 1),  cv::Point(-2, 2),  cv::Point(-1, 3)};
-    for (int y = 3; y < img.rows - 3; y++) 
+    std::vector<cv::Point> offsets = {cv::Point(0, 3),  cv::Point(1, 2),  cv::Point(2, 2),  cv::Point(3, 1),   cv::Point(3, 0),   cv::Point(3, -1),
+                                      cv::Point(2, -2), cv::Point(1, -3), cv::Point(0, -3), cv::Point(-1, -3), cv::Point(-2, -2), cv::Point(-3, -1),
+                                      cv::Point(-3, 0), cv::Point(-3, 1), cv::Point(-2, 2), cv::Point(-1, 3)};
+    for (int y = 3; y < img.rows - 3; y++)
     {
-        for (int x = 3; x < img.cols - 3; x++) 
+        for (int x = 3; x < img.cols - 3; x++)
         {
             int centerPixel = img.at<uchar>(y, x);
             std::vector<int> binaryCircle(16, 0);
@@ -42,7 +67,7 @@ void corner_detector_fast::detect(cv::InputArray image, CV_OUT std::vector<cv::K
             }
             int count = 0;
             int currentValue = binaryCircle[0];
-            for (int i = 0; i < (16 + t - 1); i++) 
+            for (int i = 0; i < (16 + t - 1); i++)
             {
                 if (currentValue == 0)
                 {
@@ -69,29 +94,37 @@ void corner_detector_fast::detect(cv::InputArray image, CV_OUT std::vector<cv::K
     }
 }
 
-void corner_detector_fast::compute(cv::InputArray, std::vector<cv::KeyPoint>& keypoints, cv::OutputArray descriptors)
+void corner_detector_fast::compute(cv::InputArray image, std::vector<cv::KeyPoint>& keypoints, cv::OutputArray descriptors)
 {
-    std::srand(unsigned(std::time(0))); // \todo remove me
-    // \todo implement any binary descriptor
     const int desc_length = 2;
-    descriptors.create(static_cast<int>(keypoints.size()), desc_length, CV_32S);
+
+    cv::Mat img;
+    image.getMat().copyTo(img);
+    if (img.channels() > 1) cv::cvtColor(img, img, cv::COLOR_BGR2GRAY);
+
+    std::vector<std::pair<cv::Point, cv::Point>> pairs;
+    const int neighbourhood_size = 25;
+    descriptors.create(static_cast<int>(keypoints.size()), desc_length, CV_8U); //  CV_8U
     auto desc_mat = descriptors.getMat();
     desc_mat.setTo(0);
 
-    int* ptr = reinterpret_cast<int*>(desc_mat.ptr());
-    for (const auto& pt : keypoints)
+    make_point_pairs(pairs, desc_length, neighbourhood_size);
+    auto test = [&img](cv::Point kp, std::pair<cv::Point, cv::Point> p) { return img.at<uint8_t>(kp + p.first) < img.at<uint8_t>(kp + p.second); };
+    auto ptr = reinterpret_cast<uint8_t*>(desc_mat.ptr());
+    for (const auto& kp : keypoints)
     {
         for (int i = 0; i < desc_length; ++i)
         {
-            *ptr = std::rand();
+            uint8_t descriptor = 0;
+            for (auto j = 0; j < pairs.size(); ++j)
+                descriptor |= (test(kp.pt, pairs.at(j)) << (pairs.size() - 1 - j));
+            *ptr = descriptor;
             ++ptr;
         }
     }
 }
 
-void corner_detector_fast::detectAndCompute(cv::InputArray img, 
-                                            cv::InputArray mask, std::vector<cv::KeyPoint>& keypoints,
-                                            cv::OutputArray descriptors,
+void corner_detector_fast::detectAndCompute(cv::InputArray img, cv::InputArray mask, std::vector<cv::KeyPoint>& keypoints, cv::OutputArray descriptors,
                                             bool useKeyPoints)
 {
     if (!useKeyPoints) detect(img, keypoints);
--- demo/demo_feature_descriptor.cpp
+++ demo/demo_feature_descriptor.cpp
@@ -4,10 +4,9 @@
  * @author Anonymous
  */
 
+#include "utils.hpp"
 #include <cvlib.hpp>
 #include <opencv2/opencv.hpp>
-
-#include "utils.hpp"
 
 int demo_feature_descriptor(int argc, char* argv[])
 {
@@ -23,30 +22,35 @@ int demo_feature_descriptor(int argc, char* argv[])
 
     cv::Mat frame;
     auto detector_a = cvlib::corner_detector_fast::create();
-    auto detector_b = cv::KAZE::create();
+    auto detector_b = cv::ORB::create();
     std::vector<cv::KeyPoint> corners;
     cv::Mat descriptors;
 
     utils::fps_counter fps;
     int pressed_key = 0;
-    while (pressed_key != 27) // ESC
+    const auto ESC_KEY_CODE = 27;
+    const auto SPACE_KEY_CODE = 32;
+    while (pressed_key != ESC_KEY_CODE)
     {
         cap >> frame;
         cv::imshow(main_wnd, frame);
 
-        detector_b->detect(frame, corners); // \todo use your detector (detector_b)
+        detector_a->detect(frame, corners);
         cv::drawKeypoints(frame, corners, frame, cv::Scalar(0, 0, 255));
-
         utils::put_fps_text(frame, fps);
-        // \todo add count of the detected corners at the top left corner of the image. Use green text color.
+
+        std::stringstream ss;
+        ss << "Corners: " << corners.size();
+        cv::putText(frame, ss.str(), cv::Point(25, 25), cv::FONT_HERSHEY_DUPLEX, 1.0, CV_RGB(0, 0, 127));
         cv::imshow(demo_wnd, frame);
 
         pressed_key = cv::waitKey(30);
         // \todo draw histogram of SSD distribution for all descriptors instead of dumping into the file
-        if (pressed_key == ' ') // space
+        if (pressed_key == SPACE_KEY_CODE)
         {
             cv::FileStorage file("descriptor.json", cv::FileStorage::WRITE | cv::FileStorage::FORMAT_JSON);
 
+            std::cout << "compute" << std::endl;
             detector_a->compute(frame, corners, descriptors);
             file << detector_a->getDefaultName() << descriptors;
 
